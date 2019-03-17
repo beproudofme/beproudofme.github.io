05aed59761da20fb0d47b26d139132109180bb7c
AleksandrChe <a.chesnokov.yur.learn@gmail.com>
2019-03-17 14:30:21 +0300

    1

--- myfirstpage/js/main.js
+++ myfirstpage/js/main.js
@@ -6,90 +6,94 @@ $(document).ready(function() {
 		$(e.target).toggleClass('done');
 	});
 
-	$('#addList').on('click', function() {
-		console.log("Содали список задач");
-		var titleTasksList = prompt("Введите название списка задач:");
-		if(titleTasksList) {
-			console.log(titleTasksList);
-			$('<li>', {
-				'class': 'list',
-				append: $('<div>', {
-					'class': 'list-header',
-					append: $('<p>', {
-						'class': 'list-header__title',
-						text: titleTasksList
-					})
-					.add ($('<button>', {
-						'class': 'list-header__deleteList btn',
-						text: "Удалить список"
-					}))
-					.add ($('<button>', {
-						'class': 'list-header__addTask btn',
-						text: "Добавить задачу",
-						on: {
-							click: function(e) {
-								console.log("Содали новую задачу");
-								var titleTask = prompt("Введите название задачи:");
-								if(titleTask) {
-									console.log(titleTask);
-									$('<li>', {
-										'class': 'task',
-										append: $('<div>', {
-											class: 'task__text',
-											append: $('<span>', {
-												text: titleTask,
-												on: {
-													click: function(e) {
-														console.log($(e.target).text());
-														$(e.target).toggleClass('done');
-													}
-												}
-											})
-										})				
-									}).appendTo($(e.target).closest('.list-header').siblings('.list-content').children('.tasks'));			
-								} else {
-									console.log("Задача не создана - не корректно задано название задачи!");
-								}
-							}
-						}
-					}))
-				})
-				.add ($('<div>', {
-					'class': 'list-content',
-					append: $('<ul>', {
-						'class': 'tasks'
-					})
-				}))
-			}).appendTo('.todo-lists');			
-		} else {
-			console.log("Список не создан - не корректно задано название списка!");
-		}
-	});
+	// $('#addList').on('click', function() {
+	// 	console.log("Содали список задач");
+	// 	var titleTasksList = prompt("Введите название списка задач:");
+	// 	if(titleTasksList) {
+	// 		console.log(titleTasksList);
+	// 		$('<li>', {
+	// 			'class': 'list',
+	// 			append: $('<div>', {
+	// 				'class': 'list-header',
+	// 				append: $('<p>', {
+	// 					'class': 'list-header__title',
+	// 					text: titleTasksList
+	// 				})
+	// 				.add ($('<button>', {
+	// 					'class': 'list-header__deleteList btn',
+	// 					text: "Удалить список"
+	// 				}))
+	// 				.add ($('<button>', {
+	// 					'class': 'list-header__addTask btn',
+	// 					text: "Добавить задачу",
+	// 					on: {
+	// 						click: function(e) {
+	// 							console.log("Содали новую задачу");
+	// 							var titleTask = prompt("Введите название задачи:");
+	// 							if(titleTask) {
+	// 								console.log(titleTask);
+	// 								$('<li>', {
+	// 									'class': 'task',
+	// 									append: $('<div>', {
+	// 										class: 'task__text',
+	// 										append: $('<span>', {
+	// 											text: titleTask,
+	// 											on: {
+	// 												click: function(e) {
+	// 													console.log($(e.target).text());
+	// 													$(e.target).toggleClass('done');
+	// 												}
+	// 											}
+	// 										})
+	// 									})				
+	// 								}).appendTo($(e.target).closest('.list-header').siblings('.list-content').children('.tasks'));			
+	// 							} else {
+	// 								console.log("Задача не создана - не корректно задано название задачи!");
+	// 							}
+	// 						}
+	// 					}
+	// 				}))
+	// 			})
+	// 			.add ($('<div>', {
+	// 				'class': 'list-content',
+	// 				append: $('<ul>', {
+	// 					'class': 'tasks'
+	// 				})
+	// 			}))
+	// 		}).appendTo('.todo-lists');			
+	// 	} else {
+	// 		console.log("Список не создан - не корректно задано название списка!");
+	// 	}
+	// });
 
-	$('.list-header__addTask').on('click', function(e) {
-		console.log("Содали новую задачу");
-		var titleTask = prompt("Введите название задачи:");
-		console.log($(e.taget).closest('.tasks'));
-		if(titleTask) {
-			console.log(titleTask);
-			$('<li>', {
-				'class': 'task',
-				append: $('<div>', {
-					class: 'task__text',
-					append: $('<span>', {
-						text: titleTask,
-						on: {
-							click: function(e) {
-								console.log($(e.target).text());
-								$(e.target).toggleClass('done');
-							}
-						}
-					})
-				})				
-			}).appendTo($(e.target).closest('.list-header').siblings('.list-content').children('.tasks'));			
-		} else {
-			console.log("Задача не создана - не корректно задано название задачи!");
-		}
+	// $('.list-header__addTask').on('click', function(e) {
+	// 	console.log("Содали новую задачу");
+	// 	var titleTask = prompt("Введите название задачи:");
+	// 	console.log($(e.taget).closest('.tasks'));
+	// 	if(titleTask) {
+	// 		console.log(titleTask);
+	// 		$('<li>', {
+	// 			'class': 'task',
+	// 			append: $('<div>', {
+	// 				class: 'task__text',
+	// 				append: $('<span>', {
+	// 					text: titleTask,
+	// 					on: {
+	// 						click: function(e) {
+	// 							console.log($(e.target).text());
+	// 							$(e.target).toggleClass('done');
+	// 						}
+	// 					}
+	// 				})
+	// 			})				
+	// 		}).appendTo($(e.target).closest('.list-header').siblings('.list-content').children('.tasks'));			
+	// 	} else {
+	// 		console.log("Задача не создана - не корректно задано название задачи!");
+	// 	}
+	// });
+
+	$('#addList').on('click', function(e) {
+		send(e);
 	});
 });
 
@@ -102,4 +106,22 @@ function show() {
 			}
 		});
 	}
+
+function send(e) {
+	console.log('Message sent to Server from "' + $(e.target).html() + '"');
+	var msg = $(e.target).html();
+	var errorMsg = "Нет ответа от сервера, сообщение не доставлено";
+	$.ajax({		
+		type: 'POST',
+		url: "http://192.168.1.2/testphp.ru/main.php",
+		data: msg,
+		cache: false,
+		success: function(data) {
+			$('.serverSay').text(data);
+		},
+		error : function(xhr, str) {
+			$('.serverSay').text(errorMsg);
+		}
+	});
+}
 
